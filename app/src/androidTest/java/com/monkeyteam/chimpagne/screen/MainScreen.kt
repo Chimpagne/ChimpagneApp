b668cdde15bd2e0c5e791e3d9847fe974157b6d7
Clément <clement.morena@epfl.ch>
2024-03-20 19:52:01 +0100

    use the given template

---
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/screen/MainScreen.kt
@@ -0,0 +1,14 @@
+package com.monkeyteam.chimpagne.screen
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import com.monkeyteam.chimpagne.resources.C
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.github.kakaocup.compose.node.element.KNode
+
+class MainScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<MainScreen>(
+        semanticsProvider = semanticsProvider,
+        viewBuilderAction = { hasTestTag(C.Tag.main_screen_container) }) {
+
+  val simpleText: KNode = child { hasTestTag(C.Tag.greeting) }
+}
