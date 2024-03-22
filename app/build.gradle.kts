6219ee3fa5e153c1d12902e44219afc226637817
Clément Moréna <clement.morena@epfl.ch>
2024-03-22 16:29:08 +0100

    secret fix ?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.google.gms.google-services")
-//    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 
     id("com.ncorti.ktfmt.gradle") version "0.16.0"
 }
@@ -72,4 +72,19 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+}
+
+secrets {
+    // Optionally specify a different file name containing your secrets.
+    // The plugin defaults to "local.properties"
+    propertiesFileName = "secrets.properties"
+
+    // A properties file containing default secret values. This file can be
+    // checked in version control.
+    defaultPropertiesFileName = "local.defaults.properties"
+
+    // Configure which keys should be ignored by the plugin by providing regular expressions.
+    // "sdk.dir" is ignored by default.
+    // ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
+    // ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
 }
