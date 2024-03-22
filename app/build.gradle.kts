31b81b471003453ccbb9769d19269d4f7b8afb30
Clément Moréna <clement.morena@epfl.ch>
2024-03-22 17:38:58 +0100

    fix type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,7 +119,7 @@ sonar {
         property("sonar.organization", "chimpagne")
         property("sonar.host.url", "https://sonarcloud.io")
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
-        property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugunitTest/")
+        property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugUnitTest/")
         // Paths to xml files with Android Lint issues. If the main flavor is changed, this file will have to be changed too.
         property("sonar.androidLint.reportPaths", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
         // Paths to JaCoCo XML coverage report files.
