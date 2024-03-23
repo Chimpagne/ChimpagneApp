d3225f10dc415aeb4b46eab69db97703df182828
Kuriv <gregory.richard@epfl.ch>
2024-03-23 10:54:19 +0100

    Files formatted now

--- app/src/main/java/com/monkeyteam/chimpagne/location/Location.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/location/Location.kt
@@ -4,19 +4,19 @@ data class Location(
     val name: String,
     val latitude: Double,
     val longitude: Double,
-){
-    constructor() :
-        this(
-            name = "",
-            latitude = 0.0,
-            longitude = 0.0,
-        )
+) {
+  constructor() :
+      this(
+          name = "",
+          latitude = 0.0,
+          longitude = 0.0,
+      )
 
-    constructor(
-        name: String
-    ) : this(
-        name = name,
-        latitude = 0.0,
-        longitude = 0.0,
-    )
+  constructor(
+      name: String
+  ) : this(
+      name = name,
+      latitude = 0.0,
+      longitude = 0.0,
+  )
 }
--- app/src/main/java/com/monkeyteam/chimpagne/location/LocationHelper.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/location/LocationHelper.kt
@@ -1,50 +1,57 @@
 package com.monkeyteam.chimpagne.model.location
 
 import android.util.Log
+import java.io.IOException
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import org.json.JSONArray
-import java.io.IOException
 
 class LocationHelper {
 
-    suspend fun convertNameToLocation(name: String, onResult: (Location) -> Unit) {
-        val client = OkHttpClient()
-        val urlSearchNominatim = "https://nominatim.openstreetmap.org/search?q=$name&format=json"
-        val request = okhttp3.Request.Builder().url(urlSearchNominatim).build()
-        client.newCall(request).enqueue(object : Callback {
+  suspend fun convertNameToLocation(name: String, onResult: (Location) -> Unit) {
+    val client = OkHttpClient()
+    val urlSearchNominatim = "https://nominatim.openstreetmap.org/search?q=$name&format=json"
+    val request = okhttp3.Request.Builder().url(urlSearchNominatim).build()
+    client
+        .newCall(request)
+        .enqueue(
+            object : Callback {
 
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e("LocationHelper", "Failed to get location for $name, because of IO Exception", e)
+              override fun onFailure(call: Call, e: IOException) {
+                Log.e(
+                    "LocationHelper",
+                    "Failed to get location for $name, because of IO Exception",
+                    e)
                 onResult(Location(name))
-            }
+              }
 
-            override fun onResponse(call: Call, response: Response) {
+              override fun onResponse(call: Call, response: Response) {
                 response.use {
-                    if (!response.isSuccessful) {
-                        Log.e("LocationHelper", "Failed to get location for $name, because of unsuccessful response")
-                        onResult(Location(name))
+                  if (!response.isSuccessful) {
+                    Log.e(
+                        "LocationHelper",
+                        "Failed to get location for $name, because of unsuccessful response")
+                    onResult(Location(name))
+                  } else {
+                    val geoLocation = response.body?.string()
+                    val jsonArray = geoLocation?.let { JSONArray(it) }
+                    if (jsonArray != null && jsonArray.length() > 0) {
+                      val jsonObject = jsonArray.getJSONObject(0)
+                      val lat = jsonObject.getDouble("lat")
+                      val lon = jsonObject.getDouble("lon")
+                      val locationInfo = Location(name, lat, lon)
+                      onResult(locationInfo)
                     } else {
-                        val geoLocation = response.body?.string()
-                        val jsonArray = geoLocation?.let { JSONArray(it) }
-                        if (jsonArray != null && jsonArray.length() > 0) {
-                            val jsonObject = jsonArray.getJSONObject(0)
-                            val lat = jsonObject.getDouble("lat")
-                            val lon = jsonObject.getDouble("lon")
-                            val locationInfo = Location(name, lat, lon)
-                            onResult(locationInfo)
-                        } else {
-                            Log.e("LocationHelper", "Failed to get location for $name, because of empty response")
-                            onResult(Location(name))
-                        }
+                      Log.e(
+                          "LocationHelper",
+                          "Failed to get location for $name, because of empty response")
+                      onResult(Location(name))
                     }
+                  }
                 }
-            }
-        }
-        )
-    }
-
-
+              }
+            })
+  }
 }
