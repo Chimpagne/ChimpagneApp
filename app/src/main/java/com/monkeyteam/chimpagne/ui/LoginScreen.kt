6de82802e44d1082298a612605dc223b4451089b
juaniac <juan.iaconucci@epfl.ch>
2024-03-25 14:41:39 +0100

    Added login fail popup as requested

--- app/src/main/java/com/monkeyteam/chimpagne/ui/LoginScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/LoginScreen.kt
@@ -8,8 +8,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +29,7 @@ import com.monkeyteam.chimpagne.ui.utilities.GoogleAuthentication
 
 @Composable
 fun LoginScreen(successfulLogin: () -> Unit) {
+  val openAlertDialog = remember { mutableStateOf(false) }
   Column(
       modifier = Modifier.fillMaxSize().padding(15.dp),
       horizontalAlignment = Alignment.CenterHorizontally,
@@ -42,6 +50,18 @@ fun LoginScreen(successfulLogin: () -> Unit) {
               fontSize = 60.sp,
           )
         }
-        GoogleAuthentication(successfulLogin, {}, Modifier.fillMaxWidth())
+        GoogleAuthentication( { successfulLogin() }, { openAlertDialog.value = true }, Modifier.fillMaxWidth())
       }
+  when {
+    openAlertDialog.value -> {
+      AlertDialog(
+          icon = { Icon(Icons.Default.Info, contentDescription = "Example Icon") },
+          title = { Text(text = "Sign in has failed") },
+          text = { Text(text = "Sign in has failed, please try again another time") },
+          onDismissRequest = { openAlertDialog.value = false },
+          confirmButton = {
+            TextButton(onClick = { openAlertDialog.value = false }) { Text("Confirm") }
+          })
+    }
+  }
 }
