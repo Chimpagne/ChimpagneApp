92d7f76eae6c38db7b9462a95dc36e09ce17929c
Athanaze <sacha.liechti@arsent.ch>
2024-03-26 09:17:46 +0000

    fixed findeventscreen formatting

--- app/src/main/java/com/monkeyteam/chimpagne/ui/FindAnEventScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/FindAnEventScreen.kt
@@ -1,14 +1,6 @@
 package com.monkeyteam.chimpagne.ui
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,7 +9,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import com.monkeyteam.chimpagne.ui.navigation.NavigationActions
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,33 +17,33 @@ fun FindAnEventScreen(navObject: NavigationActions) {
   val scope = rememberCoroutineScope()
   var showBottomSheet by remember { mutableStateOf(false) }
   /*Scaffold(
-      floatingActionButton = {
-        ExtendedFloatingActionButton(
-            text = { Text("Show bottom sheet") },
-            icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-            onClick = { showBottomSheet = true })
-      }) { contentPadding ->
-        // Screen content
+     floatingActionButton = {
+       ExtendedFloatingActionButton(
+           text = { Text("Show bottom sheet") },
+           icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+           onClick = { showBottomSheet = true })
+     }) { contentPadding ->
+       // Screen content
 
-        if (showBottomSheet) {
-          ModalBottomSheet(
-              onDismissRequest = { showBottomSheet = false }, sheetState = sheetState) {
-                // Sheet content
-                Button(
-                    onClick = {
-                      scope
-                          .launch { sheetState.hide() }
-                          .invokeOnCompletion {
-                            if (!sheetState.isVisible) {
-                              showBottomSheet = false
-                            }
-                          }
-                    }) {
-                      Text("Hide bottom sheet")
-                    }
-              }
-        }
-      }
+       if (showBottomSheet) {
+         ModalBottomSheet(
+             onDismissRequest = { showBottomSheet = false }, sheetState = sheetState) {
+               // Sheet content
+               Button(
+                   onClick = {
+                     scope
+                         .launch { sheetState.hide() }
+                         .invokeOnCompletion {
+                           if (!sheetState.isVisible) {
+                             showBottomSheet = false
+                           }
+                         }
+                   }) {
+                     Text("Hide bottom sheet")
+                   }
+             }
+       }
+     }
 
-   */
+  */
 }
