9e0fa518966440c4286b7ea0c9c83eacc41336c1
Clément Moréna <clement.morena@epfl.ch>
2024-03-26 09:35:15 +0100

    new ChimpagneEvent

--- app/src/main/java/com/monkeyteam/chimpagne/model/database/ChimpagneEvent.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/model/database/ChimpagneEvent.kt
@@ -2,10 +2,16 @@ package com.monkeyteam.chimpagne.model.database
 
 import com.monkeyteam.chimpagne.model.location.Location
 
+// https://stackoverflow.com/questions/39815117/add-an-item-to-a-list-in-firebase-database
 data class ChimpagneEvent(
     val id: String = "",
     val title: String = "",
     val description: String = "",
     val location: Location = Location("default"),
-    val isPublic: Boolean = false
-)
+    val isPublic: Boolean = false,
+    val tags: Map<String, Boolean> =  hashMapOf(),
+    val guests: Map<String, Boolean> = hashMapOf()
+) {
+    val tagList = tags.keys
+    val questList = guests.keys
+}
