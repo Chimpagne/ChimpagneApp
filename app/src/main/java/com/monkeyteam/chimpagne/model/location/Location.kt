82195c9598971ab4cd4472c8962d20f5cd543fef
Clément Moréna <clement.morena@epfl.ch>
2024-03-27 20:01:12 +0100

    location

--- app/src/main/java/com/monkeyteam/chimpagne/model/location/Location.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/model/location/Location.kt
@@ -1,7 +1,14 @@
 package com.monkeyteam.chimpagne.model.location
 
+import android.util.Log
 import com.firebase.geofire.GeoFireUtils
 import com.firebase.geofire.GeoLocation
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import org.json.JSONArray
+import java.io.IOException
 
 data class Location(
     val name: String, val latitude: Double, val longitude: Double, val geohash: String
@@ -16,7 +23,10 @@ data class Location(
         name = name, latitude = 0.0, longitude = 0.0, geohash = ""
     )
 
-    constructor(name: String, latitude: Double, longitude: Double) : this (
-        name, latitude, longitude, GeoFireUtils.getGeoHashForLocation(GeoLocation(latitude, longitude))
+    constructor(name: String, latitude: Double, longitude: Double) : this(
+        name,
+        latitude,
+        longitude,
+        GeoFireUtils.getGeoHashForLocation(GeoLocation(latitude, longitude))
     )
 }
