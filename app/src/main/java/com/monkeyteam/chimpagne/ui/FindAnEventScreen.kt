ca0e13a44ffea164c161d514f7449bb129b55083
Arnaud Levêque <arnaudlvq@gmail.com>
2024-03-28 11:58:39 +0100

    try with bottomscaffold

--- app/src/main/java/com/monkeyteam/chimpagne/ui/FindAnEventScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/FindAnEventScreen.kt
@@ -1,75 +1,75 @@
 package com.monkeyteam.chimpagne.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.monkeyteam.chimpagne.model.location.LocationHelper
 import com.monkeyteam.chimpagne.ui.navigation.NavigationActions
 import kotlinx.coroutines.launch
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FindAnEventScreen(navObject: NavigationActions) {
-    val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-    var showBottomSheet by remember { mutableStateOf(false) }
-    Scaffold(
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Show bottom sheet") },
-                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                onClick = {
-                    showBottomSheet = true
-                }
-            )
+  val scope = rememberCoroutineScope()
+  val scaffoldState = rememberBottomSheetScaffoldState()
+  BottomSheetScaffold(
+    sheetContent = {
+      Box(
+        Modifier
+          .fillMaxWidth()
+          .height(128.dp),
+        contentAlignment = Alignment.Center
+      ) {
+        Text("Swipe up to expand sheet")
+      }
+      Column(
+        Modifier
+          .fillMaxWidth()
+          .padding(64.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+        Text("Sheet content")
+        Spacer(Modifier.height(20.dp))
+        Button(
+          onClick = {
+            scope.launch { scaffoldState.bottomSheetState.partialExpand() }
+          }
+        ) {
+          Text("Click to collapse sheet")
         }
-    ) { contentPadding ->
-        // Screen content
-
-        if (showBottomSheet) {
-            ModalBottomSheet(
-                onDismissRequest = {
-                    showBottomSheet = false
-                },
-                sheetState = sheetState
-            ) {
-                // Sheet content
-                Button(onClick = {
-                    scope.launch { sheetState.hide() }.invokeOnCompletion {
-                        if (!sheetState.isVisible) {
-                            showBottomSheet = false
-                        }
-                    }
-                }) {
-                    Text("Hide bottom sheet")
-                }
-            }
-        }
+      }
+    },
+    scaffoldState = scaffoldState,
+    sheetPeekHeight = 128.dp
+  ) { innerPadding ->
+    LazyColumn(contentPadding = innerPadding) {
+      items(100) {
+        Box(
+          Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(Color.Blue)
+        )
+      }
     }
+  }
 }
-
