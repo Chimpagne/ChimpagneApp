74b9446eca045309994ec9a93c11f9a108631ae2
Arnaud Levêque <arnaudlvq@gmail.com>
2024-03-28 09:20:52 +0100

    trying bottomsheet

--- app/src/main/java/com/monkeyteam/chimpagne/ui/FindAnEventScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/FindAnEventScreen.kt
@@ -1,14 +1,21 @@
 package com.monkeyteam.chimpagne.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,41 +23,53 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.monkeyteam.chimpagne.model.location.LocationHelper
 import com.monkeyteam.chimpagne.ui.navigation.NavigationActions
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FindAnEventScreen(navObject: NavigationActions) {
-  val sheetState = rememberModalBottomSheetState()
-  val scope = rememberCoroutineScope()
-  var showBottomSheet by remember { mutableStateOf(false) }
-  Scaffold(
-      floatingActionButton = {
-        ExtendedFloatingActionButton(
-            text = { Text("Show bottom sheet") },
-            icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-            onClick = { showBottomSheet = true })
-      }) { contentPadding ->
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Show bottom sheet") },
+                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                onClick = {
+                    showBottomSheet = true
+                }
+            )
+        }
+    ) { contentPadding ->
         // Screen content
 
         if (showBottomSheet) {
-          ModalBottomSheet(
-              onDismissRequest = { showBottomSheet = false }, sheetState = sheetState) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
+            ) {
                 // Sheet content
-                Button(
-                    onClick = {
-                      scope
-                          .launch { sheetState.hide() }
-                          .invokeOnCompletion {
-                            if (!sheetState.isVisible) {
-                              showBottomSheet = false
-                            }
-                          }
-                    }) {
-                      Text("Hide bottom sheet")
+                Button(onClick = {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showBottomSheet = false
+                        }
                     }
-              }
+                }) {
+                    Text("Hide bottom sheet")
+                }
+            }
         }
-      }
+    }
 }
+
