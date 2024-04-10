2d878ae0dbcea75216aff8416a303472ae24f7b6
Arnaud Levêque <arnaudlvq@gmail.com>
2024-04-10 11:11:14 +0200

    commit before merge

--- app/src/main/java/com/monkeyteam/chimpagne/ui/components/AutoCompleteTextView.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/components/AutoCompleteTextView.kt
@@ -1,6 +1,5 @@
 package com.monkeyteam.chimpagne.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,15 +12,11 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,55 +26,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import com.monkeyteam.chimpagne.model.location.Location
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun LocationFinder(
-    locationQuery: String,
-    onLocationQueryChange: (String) -> Unit,
-    possibleLocations: List<Location>,
-    selectLocation: (Location) -> Unit,
-    modifier: Modifier = Modifier
-) {
-
-  var expanded by remember { mutableStateOf(false) }
-
-  ExposedDropdownMenuBox(
-      expanded = expanded,
-      onExpandedChange = { expanded = !expanded },
-      modifier = modifier.testTag("locationDropDownMenuBox")) {
-        TextField(
-            value = locationQuery,
-            onValueChange = onLocationQueryChange,
-            label = { Text("Location") },
-            placeholder = { Text("Enter an address") },
-            modifier =
-                Modifier.testTag("inputEventLocation")
-                    .menuAnchor()
-                    .fillMaxWidth()
-                    .background(
-                        shape = MaterialTheme.shapes.extraLarge,
-                        color = MaterialTheme.colorScheme.surfaceVariant))
-        ExposedDropdownMenu(
-            expanded = expanded,
-            onDismissRequest = {},
-            modifier = Modifier.testTag("locationFieldDropDown")) {
-              possibleLocations.forEach { location ->
-                DropdownMenuItem(
-                    onClick = {
-                      selectLocation(location)
-                      expanded = false
-                    },
-                    text = { Text(text = location.name) })
-              }
-            }
-      }
-}
 
 @Composable
 fun <T> AutoCompleteTextView(
