f075ee8e8a0ec6346515544f192ab0b62b587017
Kuriv <gregory.richard@epfl.ch>
2024-04-10 16:55:55 +0200

    New function in database to push changes to account

--- app/src/main/java/com/monkeyteam/chimpagne/model/database/ChimpagneAccountManager.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/model/database/ChimpagneAccountManager.kt
@@ -1,7 +1,9 @@
 package com.monkeyteam.chimpagne.model.database
 
+import android.net.Uri
+import android.util.Log
 import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.toObject
+import com.monkeyteam.chimpagne.model.location.Location
 
 class ChimpagneAccountManager(private val accounts: CollectionReference) {
 
@@ -13,7 +15,65 @@ class ChimpagneAccountManager(private val accounts: CollectionReference) {
     accounts
         .document(userEmail)
         .get()
-        .addOnSuccessListener { onSuccess(it.toObject<ChimpagneAccount>()) }
+        .addOnSuccessListener {
+          // Log the received data
+
+          val data = it.data
+          if (data != null) {
+            Log.d("ChimpagneAccountManager", "Received data: ${data}")
+            val email = data["email"] as String
+            val profilePictureUri = data["profilePictureUri"]?.let { Uri.parse(it as String) }
+            val firstName = data["firstName"] as String
+            val lastName = data["lastName"] as String
+            val preferredLanguageEnglish = data["preferredLanguageEnglish"] as Boolean
+            val locationMap = data["location"] as Map<*, *>
+            val location =
+                Location(
+                    name = locationMap["name"] as String,
+                    latitude = (locationMap["latitude"] as Number).toDouble(),
+                    longitude = (locationMap["longitude"] as Number).toDouble(),
+                    geohash = locationMap["geohash"] as String)
+
+            val account =
+                ChimpagneAccount(
+                    email = email,
+                    profilePictureUri = profilePictureUri,
+                    firstName = firstName,
+                    lastName = lastName,
+                    preferredLanguageEnglish = preferredLanguageEnglish,
+                    location = location)
+            onSuccess(account)
+          } else {
+            Log.d("ChimpagneAccountManager", "Received null data")
+            onSuccess(null)
+          }
+        }
+        .addOnFailureListener { onFailure(it) }
+  }
+
+  fun PutSpecificAccount(
+      account: ChimpagneAccount,
+      onSuccess: () -> Unit,
+      onFailure: (Exception) -> Unit
+  ) {
+    Log.e("ChimpagneAccountManager", "Updating account: $account")
+    val data =
+        hashMapOf(
+            "email" to account.email,
+            "profilePictureUri" to account.profilePictureUri.toString(),
+            "firstName" to account.firstName,
+            "lastName" to account.lastName,
+            "preferredLanguageEnglish" to account.preferredLanguageEnglish,
+            "location" to
+                hashMapOf(
+                    "name" to account.location?.name,
+                    "latitude" to account.location?.latitude,
+                    "longitude" to account.location?.longitude,
+                    "geohash" to account.location?.geohash))
+    accounts
+        .document(account.email)
+        .update(data as Map<String, Any>)
+        .addOnSuccessListener { onSuccess() }
         .addOnFailureListener { onFailure(it) }
   }
 }
