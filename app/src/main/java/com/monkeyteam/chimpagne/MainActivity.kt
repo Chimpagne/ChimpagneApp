deb89ce7560592c111154053570dd84c89bde827
Clément <clement.morena@epfl.ch>
2024-04-24 13:54:03 +0200

    new Database

--- app/src/main/java/com/monkeyteam/chimpagne/MainActivity.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/MainActivity.kt
@@ -11,11 +11,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import com.monkeyteam.chimpagne.model.database.Database
 import com.monkeyteam.chimpagne.ui.AccountEdit
 import com.monkeyteam.chimpagne.ui.EventCreationScreen
 import com.monkeyteam.chimpagne.ui.HomeScreen
@@ -27,32 +32,34 @@ import com.monkeyteam.chimpagne.ui.theme.AccountCreation
 import com.monkeyteam.chimpagne.ui.theme.ChimpagneTheme
 import com.monkeyteam.chimpagne.ui.utilities.SpinnerView
 import com.monkeyteam.chimpagne.viewmodels.AccountViewModel
+import com.monkeyteam.chimpagne.viewmodels.AccountViewModelFactory
+import com.monkeyteam.chimpagne.viewmodels.EventViewModel
+import com.monkeyteam.chimpagne.viewmodels.EventViewModelFactory
+import com.monkeyteam.chimpagne.viewmodels.FindEventsViewModelFactory
 
 class MainActivity : ComponentActivity() {
+
+  val database = Database()
+  val accountViewModel: AccountViewModel by viewModels { AccountViewModelFactory(database.accountManager) }
+
   @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    val accountViewModel: AccountViewModel by viewModels()
-
     setContent {
       ChimpagneTheme {
         val navController = rememberNavController()
         val navActions = NavigationActions(navController)
 
         val loginToChimpagneAccount: (id: String) -> Unit = { id ->
-          accountViewModel.loginToChimpagneAccount(
-              id,
-              { account ->
-                if (account != null) {
-                  Log.d("MainActivity", "Account is in database")
-                  navActions.clearAndNavigateTo(Route.HOME_SCREEN, true)
-                } else {
-                  Log.d("MainActivity", "Account is not in database")
-                  navActions.clearAndNavigateTo(Route.ACCOUNT_CREATION_SCREEN)
-                }
-              },
-              { Log.e("MainActivity", "Failed to check if account is in database: $it") })
+          accountViewModel.loginToChimpagneAccount(id, { account ->
+            if (account != null) {
+              Log.d("MainActivity", "Account is in database")
+              navActions.clearAndNavigateTo(Route.HOME_SCREEN, true)
+            } else {
+              Log.d("MainActivity", "Account is not in database")
+              navActions.clearAndNavigateTo(Route.ACCOUNT_CREATION_SCREEN)
+            }
+          }, { Log.e("MainActivity", "Failed to check if account is in database: $it") })
         }
 
         val logout: () -> Unit = {
@@ -63,13 +70,12 @@ class MainActivity : ComponentActivity() {
 
         // Determine the start destination based on the isAuthenticated state
         // Using null check to decide, assuming that null means the auth state is still being determined
-        val startDestination =
-            if (FirebaseAuth.getInstance().currentUser != null) {
-              loginToChimpagneAccount(FirebaseAuth.getInstance().currentUser?.uid!!)
-              Route.LOADING
-            } else {
-              Route.LOGIN_SCREEN
-            }
+        val startDestination = if (FirebaseAuth.getInstance().currentUser != null) {
+          loginToChimpagneAccount(FirebaseAuth.getInstance().currentUser?.uid!!)
+          Route.LOADING
+        } else {
+          Route.LOGIN_SCREEN
+        }
 
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
           NavHost(navController = navController, startDestination = startDestination) {
@@ -81,7 +87,8 @@ class MainActivity : ComponentActivity() {
             }
             composable(Route.ACCOUNT_SETTINGS_SCREEN) {
               AccountSettings(
-                  navObject = navActions, accountViewModel = accountViewModel, logout = logout)
+                navObject = navActions, accountViewModel = accountViewModel, logout = logout
+              )
             }
             composable(Route.ACCOUNT_EDIT_SCREEN) {
               AccountEdit(navObject = navActions, accountViewModel = accountViewModel)
@@ -89,8 +96,18 @@ class MainActivity : ComponentActivity() {
 
             composable(Route.LOADING) { SpinnerView() }
             composable(Route.HOME_SCREEN) { HomeScreen(navObject = navActions) }
-            composable(Route.FIND_AN_EVENT_SCREEN) { MainFindEventScreen(navObject = navActions) }
-            composable(Route.EVENT_CREATION_SCREEN) { EventCreationScreen(navObject = navActions) }
+            composable(Route.FIND_AN_EVENT_SCREEN) {
+              MainFindEventScreen(
+                navObject = navActions,
+                findViewModel = viewModel(factory = FindEventsViewModelFactory(database.eventManager))
+              )
+            }
+            composable(Route.EVENT_CREATION_SCREEN) {
+              EventCreationScreen(
+                navObject = navActions,
+                eventViewModel = viewModel(factory = EventViewModelFactory(null, database.eventManager))
+              )
+            }
           }
         }
       }
