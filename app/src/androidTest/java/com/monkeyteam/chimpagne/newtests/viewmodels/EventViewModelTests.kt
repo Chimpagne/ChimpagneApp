6e2370651bc8730f1564e4500c3fc35499273915
Athanaze <sacha.liechti@arsent.ch>
2024-04-25 12:13:23 +0100

    Fixed merge conflict

--- app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/viewmodels/EventViewModelTests.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/viewmodels/EventViewModelTests.kt
@@ -103,6 +103,7 @@ class EventViewModelTests {
         while (eventSearchVM.uiState.value.loading) {}
 
 
+
         assertTrue(eventSearchVM.uiState.value.title == testEvent.title)
         assertTrue(eventSearchVM.uiState.value.description == testEvent.description)
         assertTrue(eventSearchVM.uiState.value.location.name == testEvent.location.name)
