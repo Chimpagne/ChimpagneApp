13f3d2cff56a2c5816015c48998a38a876ea556e
Lea Grieder <lea@grieder.org>
2024-04-26 14:38:51 +0200

    Refactor search bar behavior as per Clement's request                                                                                                                         I modified the logic so the search bar does not display upon typing. I also added a Toast notification for cases where no locations are found from Nominatim.

--- app/src/main/java/com/monkeyteam/chimpagne/ui/components/LocationSelector.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/components/LocationSelector.kt
@@ -1,5 +1,8 @@
 package com.monkeyteam.chimpagne.ui.components
 
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,7 +13,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -26,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -41,27 +44,37 @@ fun LocationSelector(
     modifier: Modifier = Modifier
 ) {
 
-  var locationQuery by remember { mutableStateOf(selectedLocation?.name ?: "") }
-  var searching by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    var locationQuery by remember { mutableStateOf(selectedLocation?.name ?: "") }
+    var searching by remember { mutableStateOf(false) }
     var showSearchBar by remember { mutableStateOf(false) }
-  var searchCompleted by remember { mutableStateOf(false) }
-  var possibleLocations by remember { mutableStateOf(emptyList<Location>()) }
+    var searchCompleted by remember { mutableStateOf(false) }
+    var possibleLocations by remember { mutableStateOf(emptyList<Location>()) }
 
-  val launchSearch = {
-      if(locationQuery.isNotEmpty()){
-          searching = true
-          convertNameToLocations(
-              locationQuery,
-              {
-                  possibleLocations = it
-                  searching = false
-                  searchCompleted = it.isNotEmpty()
-                  showSearchBar = it.isNotEmpty()
-              },
-              10
-          )
-      }
-  }
+    val launchSearch = {
+        if(locationQuery.isNotEmpty()){
+            searching = true
+            convertNameToLocations(
+                locationQuery,
+                {
+                    possibleLocations = it
+                    searching = false
+                    showSearchBar = it.isNotEmpty()
+                    searchCompleted = it.isNotEmpty()
+                    Handler(Looper.getMainLooper()).post {
+                        if (it.isEmpty()) {
+                            Toast.makeText(
+                                context,
+                                context.getString(R.string.find_event_no_result),
+                                Toast.LENGTH_SHORT
+                            ).apply { show() }
+                        }
+                    }
+                },
+                10
+            )
+        }
+    }
 
     val clearLocationInput = {
         locationQuery = ""
@@ -71,29 +84,25 @@ fun LocationSelector(
         updateSelectedLocation(Location())
     }
 
-
     DockedSearchBar(
-      query = locationQuery,
-      onQueryChange = {
-          locationQuery = it
-          searchCompleted= false
-          if (it.isNotEmpty()) {
-              showSearchBar = true
-          }
-      },
-      onSearch = { launchSearch() },
-      active = showSearchBar,
+        query = locationQuery,
+        onQueryChange = {
+            locationQuery = it
+            searchCompleted= false
+        },
+        onSearch = { launchSearch() },
+        active = showSearchBar,
         onActiveChange = {
             if (!it) {
                 if (locationQuery.isEmpty() && !searching) {
                     showSearchBar = false
                 }
             } else {
-                showSearchBar = searching || searchCompleted || locationQuery.isNotEmpty()
+                showSearchBar = searching ||  searchCompleted || locationQuery.isNotEmpty()
             }
         },
-      placeholder = { Text(stringResource(id = R.string.search_location)) },
-      modifier = modifier.testTag("LocationComponent"),
+        placeholder = { Text(stringResource(id = R.string.search_location)) },
+        modifier = modifier.testTag("LocationComponent"),
         trailingIcon = {
             when {
                 searching -> CircularProgressIndicator()
