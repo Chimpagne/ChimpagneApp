42b092e0393bea4b4891026d186be9d23e74cdaf
Clément <clement.morena@epfl.ch>
2024-05-01 18:00:44 +0200

    progress

---
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/eventdetails/supplies/SuppliesScreen.kt
@@ -0,0 +1,16 @@
+package com.monkeyteam.chimpagne.ui.eventdetails.supplies
+
+import androidx.compose.runtime.Composable
+import com.monkeyteam.chimpagne.model.database.ChimpagneRole
+import com.monkeyteam.chimpagne.viewmodels.EventViewModel
+
+@Composable
+fun SuppliesScreen(
+  eventViewModel: EventViewModel,
+  userRole: ChimpagneRole = ChimpagneRole.GUEST
+) {
+
+
+
+
+}
---
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/eventdetails/supplies/SuppyGuestDialog.kt
@@ -0,0 +1,8 @@
+package com.monkeyteam.chimpagne.ui.eventdetails.supplies
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SupplyGuestDialog() {
+  
+}
