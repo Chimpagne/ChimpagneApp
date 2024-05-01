0c62da0ffdd87e5d6b14d8dbb3f4c25eccce660f
Clément <clement.morena@epfl.ch>
2024-05-01 20:03:46 +0200

    progress

--- app/src/main/java/com/monkeyteam/chimpagne/ui/eventdetails/supplies/SuppliesScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/eventdetails/supplies/SuppliesScreen.kt
@@ -1,16 +1,20 @@
 package com.monkeyteam.chimpagne.ui.eventdetails.supplies
 
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.monkeyteam.chimpagne.model.database.ChimpagneRole
 import com.monkeyteam.chimpagne.viewmodels.EventViewModel
 
 @Composable
 fun SuppliesScreen(
   eventViewModel: EventViewModel,
-  userRole: ChimpagneRole = ChimpagneRole.GUEST
 ) {
-
-
-
+  val uiState by eventViewModel.uiState.collectAsState()
+  
+  Button(onClick = {  }) {
+    
+  }
 
 }
