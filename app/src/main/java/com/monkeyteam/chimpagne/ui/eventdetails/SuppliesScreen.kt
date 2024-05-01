e469e74623bd14eec8fd4753f8ce53fb9a38e0d8
Clément <clement.morena@epfl.ch>
2024-05-01 16:38:29 +0200

    supplies screen creation

---
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/eventdetails/SuppliesScreen.kt
@@ -0,0 +1,13 @@
+package com.monkeyteam.chimpagne.ui.eventdetails
+
+import androidx.compose.runtime.Composable
+import com.monkeyteam.chimpagne.model.database.ChimpagneRole
+import com.monkeyteam.chimpagne.viewmodels.EventViewModel
+
+@Composable
+fun SuppliesScreen(
+  eventViewModel: EventViewModel,
+  userRole: ChimpagneRole = ChimpagneRole.GUEST
+) {
+
+}
