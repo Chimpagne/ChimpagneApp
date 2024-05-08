3bec3032aca133b77c2a6d998540e2852d813fe5
Clément <clement.morena@epfl.ch>
2024-05-08 16:31:33 +0200

    + tests

--- app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/model/event/EventTest.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/model/event/EventTest.kt
@@ -2,12 +2,36 @@ package com.monkeyteam.chimpagne.newtests.model.event
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.monkeyteam.chimpagne.model.database.ChimpagneEvent
+import com.monkeyteam.chimpagne.model.database.ChimpagneSupply
+import com.monkeyteam.chimpagne.model.database.Database
+import com.monkeyteam.chimpagne.newtests.TEST_ACCOUNTS
+import com.monkeyteam.chimpagne.newtests.TEST_EVENTS
+import com.monkeyteam.chimpagne.newtests.initializeTestDatabase
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNull
+import junit.framework.TestCase.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class EventTest {
+
+  val database = Database()
+  val accountManager = database.accountManager
+  val eventManager = database.eventManager
+
+//  val ownerOfEvent0 = TEST_ACCOUNTS[1]
+  val event = TEST_EVENTS[0]
+  val account0 = TEST_ACCOUNTS[0]
+  val account1 = TEST_ACCOUNTS[1]
+  val account2 = TEST_ACCOUNTS[2]
+
+  @Before
+  fun init() {
+    initializeTestDatabase()
+    accountManager.signInTo((account0))
+  }
 
   @Test
   fun guestListAndStaffListTest() {
@@ -16,5 +40,57 @@ class EventTest {
             staffs = hashMapOf("1" to true, "2" to true), guests = hashMapOf("3" to true))
     assertEquals(event.guests.keys, event.guestList())
     assertEquals(event.staffs.keys, event.staffList())
+  }
+
+  @Test
+  fun supplyTest() {
+    val supply = ChimpagneSupply(
+      description = "bananas from la Migros",
+      quantity = 4,
+      unit = "bananas",
+      assignedTo = hashMapOf(account0.firebaseAuthUID to true, account1.firebaseAuthUID to true)
+    )
+
+    var loading = true
+    eventManager.atomic.updateSupply(event.id, supply, { loading = false }, { assertTrue(false) })
+    while (loading)  {}
+    var updatedEvent = ChimpagneEvent()
+    loading = true
+    eventManager.getEventById(event.id, { updatedEvent = it!!; loading = false }, { assertTrue(false) })
+    while (loading)  {}
+
+    assertEquals(supply, updatedEvent.supplies[supply.id])
+
+    loading = true
+    eventManager.atomic.removeSupply(event.id, supply.id, { loading = false }, { assertTrue(false) })
+    while (loading)  {}
+    updatedEvent = ChimpagneEvent()
+    loading = true
+    eventManager.getEventById(event.id, { updatedEvent = it!!; loading = false }, { assertTrue(false) })
+    while (loading)  {}
+
+    assertEquals(null, updatedEvent.supplies[supply.id])
+
+    loading = true
+    eventManager.atomic.updateSupply(event.id, supply, { loading = false }, { assertTrue(false) })
+    while (loading)  {}
+    updatedEvent = ChimpagneEvent()
+    loading = true
+    eventManager.atomic.assignSupply(event.id, supply.id, account2.firebaseAuthUID,  { loading = false }, { assertTrue(false) })
+    while (loading) {}
+    loading = true
+    eventManager.getEventById(event.id, { updatedEvent = it!!; loading = false }, { assertTrue(false) })
+    while (loading)  {}
+
+    assertTrue(updatedEvent.supplies[supply.id]!!.assignedTo[account2.firebaseAuthUID] == true)
+
+    loading = true
+    eventManager.atomic.unassignSupply(event.id, supply.id, account2.firebaseAuthUID,  { loading = false }, { assertTrue(false) })
+    while (loading) {}
+    loading = true
+    eventManager.getEventById(event.id, { updatedEvent = it!!; loading = false }, { assertTrue(false) })
+    while (loading)  {}
+
+    assertNull(updatedEvent.supplies[supply.id]!!.assignedTo[account2.firebaseAuthUID])
   }
 }
