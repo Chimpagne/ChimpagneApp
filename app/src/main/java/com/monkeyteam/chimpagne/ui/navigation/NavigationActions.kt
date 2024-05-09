71241e49e4820971503fa56933ef8c0b380fdefb
Clément <clement.morena@epfl.ch>
2024-05-09 22:39:55 +0200

    ktfmtification

--- app/src/main/java/com/monkeyteam/chimpagne/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/navigation/NavigationActions.kt
@@ -1,6 +1,5 @@
 package com.monkeyteam.chimpagne.ui.navigation
 
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 
 object Route {
@@ -25,13 +24,13 @@ class NavigationActions(private val navController: NavHostController) {
       // avoid building up a large stack of destinations
       // on the back stack as users select items
 
-//      popUpTo(navController.graph.findStartDestination().id) { saveState = true }
+      //      popUpTo(navController.graph.findStartDestination().id) { saveState = true }
 
       // Avoid multiple copies of the same destination when
       // reselecting the same item
       launchSingleTop = true
       // Restore state when reselecting a previously selected item
-//      restoreState = true
+      //      restoreState = true
     }
   }
 
