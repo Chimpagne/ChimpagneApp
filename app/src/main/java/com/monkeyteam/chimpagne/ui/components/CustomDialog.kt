26c6cade71b757ed481f1415cf7033f09a965c6f
Clément <clement.morena@epfl.ch>
2024-05-12 21:39:48 +0200

    new add/edit supply dialog

---
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/components/CustomDialog.kt
@@ -0,0 +1,72 @@
+package com.monkeyteam.chimpagne.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun CustomDialog(
+  title: String,
+  description: String,
+  onDismissRequest: () -> Unit,
+  buttonDataList: List<ButtonData>,
+  modifier: Modifier = Modifier,
+  content: @Composable () -> Unit = {}
+) {
+  Dialog(onDismissRequest = onDismissRequest) {
+    Card(
+      modifier = modifier.fillMaxWidth().padding(16.dp),
+      shape = AlertDialogDefaults.shape,
+    ) {
+      Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+          title,
+          modifier = Modifier.fillMaxWidth(),
+          style =
+          TextStyle(
+            fontSize = 24.sp,
+            lineHeight = 32.sp,
+            fontWeight = FontWeight(400),
+            color = AlertDialogDefaults.titleContentColor,
+          )
+        )
+        Text(
+          description,
+          modifier = Modifier.fillMaxWidth().padding(0.dp, 7.dp),
+          style =
+          TextStyle(
+            fontSize = 16.sp,
+            lineHeight = 20.sp,
+            fontWeight = FontWeight(400),
+            color = AlertDialogDefaults.textContentColor,
+            letterSpacing = 0.25.sp,
+          ))
+        content()
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+          buttonDataList.forEach {
+            TextButton(onClick = it.onClick, modifier = it.modifier) { Text(it.text) }
+          }
+        }
+      }
+    }
+  }
+}
+
+data class ButtonData(val text: String, val modifier: Modifier = Modifier, val onClick: () -> Unit)
---
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/event/details/supplies/EditSupplyDialog.kt
@@ -0,0 +1,88 @@
+package com.monkeyteam.chimpagne.ui.event.details.supplies
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.monkeyteam.chimpagne.R
+import com.monkeyteam.chimpagne.model.database.ChimpagneSupply
+import com.monkeyteam.chimpagne.ui.components.ButtonData
+import com.monkeyteam.chimpagne.ui.components.CustomDialog
+
+@Composable
+fun EditSupplyDialog(
+  supply: ChimpagneSupply,
+  onDismissRequest: () -> Unit,
+  onSave: (ChimpagneSupply) -> Unit,
+  title: String = stringResource(id = R.string.supplies_add_dialog_title),
+  saveButton: String = stringResource(id = R.string.chimpagne_add)
+) {
+  var description by remember { mutableStateOf(supply.description) }
+  var quantity by remember { mutableStateOf(supply.quantity.toString()) }
+  var unit by remember { mutableStateOf(supply.unit) }
+
+  CustomDialog(
+    title = title,
+    description = stringResource(id = R.string.supplies_dialog_description),
+    onDismissRequest = onDismissRequest,
+    modifier = Modifier.testTag("edit_supply_dialog"),
+    buttonDataList =
+    listOf(
+      ButtonData(
+        stringResource(id = R.string.chimpagne_cancel), onClick = onDismissRequest),
+      ButtonData(saveButton, modifier = Modifier.testTag("supplies_add_button")) {
+        onSave(
+          supply.copy(
+            description = description,
+            quantity = quantity.toIntOrNull() ?: 0,
+            unit = unit))
+        onDismissRequest()
+      })) {
+    Row(Modifier.fillMaxWidth()) {
+      OutlinedTextField(
+        modifier = Modifier.weight(0.25f).padding(5.dp).testTag("supplies_quantity_field"),
+        maxLines = 1,
+        value = quantity,
+        onValueChange = {
+          if ((it.toIntOrNull() ?: -1) > 0) {
+            if (quantity == "0") {
+              if (it.first() == '0') quantity = it.drop(1) else quantity = it.dropLast(1)
+            } else quantity = it
+          }
+          if (it == "") quantity = "0"
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardActions = KeyboardActions(onDone = { /* Handle Done action */}),
+        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+        placeholder = { Text(stringResource(id = R.string.supplies_quantity)) })
+      OutlinedTextField(
+        modifier = Modifier.weight(0.75f).padding(5.dp).testTag("supplies_unit_field"),
+        maxLines = 1,
+        value = unit,
+        onValueChange = { unit = it },
+        placeholder = { Text(stringResource(id = R.string.supplies_unit)) })
+    }
+
+    OutlinedTextField(
+      modifier = Modifier.fillMaxWidth().padding(5.dp).testTag("supplies_description_field"),
+      maxLines = 1,
+      value = description,
+      onValueChange = { description = it },
+      placeholder = { Text(stringResource(id = R.string.supplies_description)) })
+  }
+}
