cac2c3a044e413cbbcf10d4017dc8d1c7a2e0ce9
Clément <clement.morena@epfl.ch>
2024-05-12 22:13:57 +0200

    +ktfmtification

--- app/src/test/java/com/monkeyteam/chimpagne/model/ChimpagneEventTest.kt
+++ app/src/test/java/com/monkeyteam/chimpagne/model/ChimpagneEventTest.kt
@@ -8,7 +8,11 @@ class ChimpagneEventTest {
 
   @Test
   fun userSetTest() {
-    val event = ChimpagneEvent(ownerId = "banana", staffs = mapOf("1" to true, "2" to true, "3" to true), guests = mapOf("4" to true))
+    val event =
+        ChimpagneEvent(
+            ownerId = "banana",
+            staffs = mapOf("1" to true, "2" to true, "3" to true),
+            guests = mapOf("4" to true))
     assertEquals(setOf("banana", "1", "2", "3", "4"), event.getUserSet())
   }
 }
