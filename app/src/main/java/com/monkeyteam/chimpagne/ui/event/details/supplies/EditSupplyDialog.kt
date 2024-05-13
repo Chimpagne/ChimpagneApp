6a36e81b8d92e4aac3c7a2b7223e504fc3345103
Clément <clement.morena@epfl.ch>
2024-05-13 19:53:53 +0200

    ktfmtification

--- app/src/main/java/com/monkeyteam/chimpagne/ui/event/details/supplies/EditSupplyDialog.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/event/details/supplies/EditSupplyDialog.kt
@@ -3,7 +3,6 @@ package com.monkeyteam.chimpagne.ui.event.details.supplies
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.OutlinedTextField
