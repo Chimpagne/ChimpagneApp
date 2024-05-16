d95efce05f885159e0fac7da2ba136ab89d8bffa
Clément <clement.morena@epfl.ch>
2024-05-16 22:01:44 +0200

    address changes

--- app/src/main/java/com/monkeyteam/chimpagne/ui/components/DateRangeSelector.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/components/DateRangeSelector.kt
@@ -3,9 +3,12 @@ package com.monkeyteam.chimpagne.ui.components
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.CalendarToday
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.DateRangePickerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +16,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
@@ -39,7 +44,9 @@ fun DateRangeSelector(
 
   IconTextButton(
       text =
-          "${DateFormat.getDateInstance(DateFormat.MEDIUM).format(startDate.time)} - ${DateFormat.getDateInstance(DateFormat.MEDIUM).format(endDate.time)}",
+          "${
+      DateFormat.getDateInstance(DateFormat.MEDIUM).format(startDate.time)
+    } - ${DateFormat.getDateInstance(DateFormat.MEDIUM).format(endDate.time)}",
       icon = Icons.Rounded.CalendarToday,
       onClick = { showDialog = true },
       modifier = modifier.testTag("date_range_button"))
@@ -68,8 +75,6 @@ private fun DateRangeSelectorDialog(
               })
 
   CustomDialog(
-      title = stringResource(id = R.string.select_date_range_for_query),
-      description = "",
       onDismissRequest = onDismissRequest,
       buttonDataList =
           listOf(
@@ -94,6 +99,19 @@ private fun DateRangeSelectorDialog(
         DateRangePicker(
             state = dateRangeState,
             showModeToggle = false,
+            title = { Text(stringResource(id = R.string.select_date_range_for_query)) },
+            headline = {
+              DateRangePickerDefaults.DateRangePickerHeadline(
+                  selectedStartDateMillis = dateRangeState.selectedStartDateMillis,
+                  selectedEndDateMillis = dateRangeState.selectedEndDateMillis,
+                  displayMode = dateRangeState.displayMode,
+                  dateFormatter =
+                      DatePickerDefaults.dateFormatter("yy MM dd", "yy MM dd", "yy MM dd"))
+            },
+            colors =
+                DatePickerDefaults.colors(
+                    dayInSelectionRangeContainerColor = Color.Transparent,
+                    dayInSelectionRangeContentColor = DatePickerDefaults.colors().dayContentColor),
             modifier = Modifier.height(LocalConfiguration.current.screenHeightDp.dp.times(0.60f)))
       }
 }
