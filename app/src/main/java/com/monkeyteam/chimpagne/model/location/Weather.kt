9e0c794f471d4a850cb49f05f8a94661cad091f7
Kuriv <gregory.richard@epfl.ch>
2024-05-16 16:31:47 +0200

    Updated weather class

--- app/src/main/java/com/monkeyteam/chimpagne/model/location/Weather.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/model/location/Weather.kt
@@ -2,9 +2,9 @@ package com.monkeyteam.chimpagne.model.location
 
 data class Weather(
     val location: Location? = Location(),
+    val weatherDescription: String = "",
+    val weatherIcon: String = "",
     val temperatureLow: Double = 0.0,
     val temperatureHigh: Double = 0.0,
     val maxWindSpeed: Double = 0.0,
-    val weatherDescription: String = "",
-    val weatherIcon: String = "",
-    )
+)
