d0b7d1f87537135473acccf1f84116f8bf15de71
Athanaze <sacha.liechti@arsent.ch>
2024-05-16 17:49:59 +0200

    ktftmt

--- app/src/main/java/com/monkeyteam/chimpagne/ui/HomeScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/HomeScreen.kt
@@ -48,7 +48,6 @@ import com.monkeyteam.chimpagne.ui.components.EventCard
 import com.monkeyteam.chimpagne.ui.components.ProfileIcon
 import com.monkeyteam.chimpagne.ui.navigation.NavigationActions
 import com.monkeyteam.chimpagne.ui.navigation.Route
-import com.monkeyteam.chimpagne.ui.theme.ChimpagneTypography
 import com.monkeyteam.chimpagne.ui.utilities.PromptLogin
 import com.monkeyteam.chimpagne.viewmodels.AccountViewModel
 import com.monkeyteam.chimpagne.viewmodels.FindEventsViewModel
@@ -131,7 +130,6 @@ fun HomeScreen(
             modifier =
                 Modifier.fillMaxSize()
                     .background(MaterialTheme.colorScheme.background)
-
                     .padding(innerPadding)) {
               // Events Feed taking the top half of the screen
               LazyColumn(
@@ -151,7 +149,6 @@ fun HomeScreen(
                           onClick = {
                             navObject.navigateTo(Route.VIEW_DETAIL_EVENT_SCREEN + "/${event.id}")
                           })
-
                     }
                   }
 
