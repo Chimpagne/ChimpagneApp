7cb4e3a3846c94ee853647a4cf5ebc7f7f08a600
Arnaud Levêque <arnaudlvq@gmail.com>
2024-05-16 18:07:47 +0200

    ktfmt

--- app/src/androidTest/java/com/monkeyteam/chimpagne/MapTest.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/MapTest.kt
@@ -1,6 +1,5 @@
 package com.monkeyteam.chimpagne
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
