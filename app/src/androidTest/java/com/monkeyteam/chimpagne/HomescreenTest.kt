936d9686b9379f8e47cdb560fa34b1fc29944a3e
Lea Grieder <lea@grieder.org>
2024-05-17 13:37:09 +0200

    broken test

--- app/src/androidTest/java/com/monkeyteam/chimpagne/HomescreenTest.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/HomescreenTest.kt
@@ -5,6 +5,11 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiObject
+import androidx.test.uiautomator.UiObjectNotFoundException
+import androidx.test.uiautomator.UiSelector
 import com.monkeyteam.chimpagne.model.database.Database
 import com.monkeyteam.chimpagne.ui.HomeScreen
 import com.monkeyteam.chimpagne.ui.navigation.NavigationActions
@@ -19,8 +24,21 @@ class HomescreenTest {
   @get:Rule val composeTestRule = createComposeRule()
   val database = Database()
 
+  fun allowPermissionsIfNeeded() {
+    val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    try {
+      val allowPermissions: UiObject = uiDevice.findObject(UiSelector().text("ALLOW"))
+      if (allowPermissions.exists()) {
+        allowPermissions.click()
+      }
+    } catch (e: UiObjectNotFoundException) {
+      // Handle the exception
+    }
+  }
+
   @Test
   fun TestButtonsAreDisplayed() {
+    allowPermissionsIfNeeded()
     val accountViewModel = AccountViewModel(database = database)
     // Start on the correct screen
     composeTestRule.setContent {
@@ -28,8 +46,6 @@ class HomescreenTest {
       val navActions = NavigationActions(navController)
       HomeScreen(navActions, accountViewModel)
     }
-
-    composeTestRule.waitForIdle()
 
     composeTestRule.onNodeWithTag("open_events_button").assertIsDisplayed()
     composeTestRule.onNodeWithTag("discover_events_button").assertIsDisplayed()
