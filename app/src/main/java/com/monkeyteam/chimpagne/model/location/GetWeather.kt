4ccc8b7db932b1b5d23d2c0ae02eb96894c041a5
Kuriv <gregory.richard@epfl.ch>
2024-05-17 08:19:23 +0200

    Added tests

--- app/src/main/java/com/monkeyteam/chimpagne/model/location/GetWeather.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/model/location/GetWeather.kt
@@ -1,6 +1,8 @@
 package com.monkeyteam.chimpagne.model.location
 
 import android.util.Log
+import java.io.IOException
+import java.time.LocalDate
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.HttpUrl
@@ -8,8 +10,14 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONObject
-import java.io.IOException
-import java.time.LocalDate
+
+object GetWeatherConstants {
+  var PORT = 443
+  var SCHEME = "https"
+  var HOST = "api.weatherapi.com"
+  var VERSION = "v1"
+  var FORECAST = "forecast.json"
+}
 
 fun getWeather(
     location: Location,
@@ -22,10 +30,11 @@ fun getWeather(
 
   val freeWeatherApiUrl =
       HttpUrl.Builder()
-          .scheme("https")
-          .host("api.weatherapi.com")
-          .addPathSegment("v1")
-          .addPathSegment("forecast.json")
+          .port(GetWeatherConstants.PORT)
+          .scheme(GetWeatherConstants.SCHEME)
+          .host(GetWeatherConstants.HOST)
+          .addPathSegment(GetWeatherConstants.VERSION)
+          .addPathSegment(GetWeatherConstants.FORECAST)
           .addQueryParameter("key", apiKey)
           .addQueryParameter("q", "${location.latitude},${location.longitude}")
           .addQueryParameter("dt", date.toString())
@@ -61,42 +70,35 @@ fun getWeather(
                   } else {
                     try {
                       val weatherResponse = JSONObject(jsonString)
-                      if (weatherResponse.getJSONObject("location").getDouble("lat") ==
-                          location.latitude &&
-                          weatherResponse.getJSONObject("location").getDouble("lon") ==
-                              location.longitude) {
-                        val forecastDay =
-                            weatherResponse
-                                .getJSONObject("forecast")
-                                .getJSONArray("forecastday")
-                                .getJSONObject(0)
+                      val forecastDay =
+                          weatherResponse
+                              .getJSONObject("forecast")
+                              .getJSONArray("forecastday")
+                              .getJSONObject(0)
 
-                        val weather =
-                            Weather(
-                                location = location,
-                                weatherDescription =
-                                forecastDay
-                                    .getJSONObject("day")
-                                    .getJSONObject("condition")
-                                    .getString("text"),
-                                weatherIcon =
-                                forecastDay
-                                    .getJSONObject("day")
-                                    .getJSONObject("condition")
-                                    .getString("icon"),
-                                temperatureLow =
-                                    forecastDay.getJSONObject("day").getDouble("mintemp_c"),
-                                temperatureHigh =
-                                    forecastDay.getJSONObject("day").getDouble("maxtemp_c"),
-                                maxWindSpeed =
-                                    forecastDay.getJSONObject("day").getDouble("maxwind_kph"),
-                                windDirection = forecastDay.getJSONObject("day").getString("wind_dir"))
+                      val weather =
+                          Weather(
+                              location = location,
+                              weatherDescription =
+                                  forecastDay
+                                      .getJSONObject("day")
+                                      .getJSONObject("condition")
+                                      .getString("text"),
+                              weatherIcon =
+                                  forecastDay
+                                      .getJSONObject("day")
+                                      .getJSONObject("condition")
+                                      .getString("icon"),
+                              temperatureLow =
+                                  forecastDay.getJSONObject("day").getDouble("mintemp_c"),
+                              temperatureHigh =
+                                  forecastDay.getJSONObject("day").getDouble("maxtemp_c"),
+                              maxWindSpeed =
+                                  forecastDay.getJSONObject("day").getDouble("maxwind_kph"),
+                              windDirection =
+                                  forecastDay.getJSONObject("day").optString("wind_dir", "NA"))
 
-                        onSuccess(weather)
-                      } else {
-                        Log.e("WeatherHelper", "Json response does not match location")
-                        onFailure()
-                      }
+                      onSuccess(weather)
                     } catch (e: Exception) {
                       Log.e("WeatherHelper", "Invalid JSON response", e)
                       onFailure()
