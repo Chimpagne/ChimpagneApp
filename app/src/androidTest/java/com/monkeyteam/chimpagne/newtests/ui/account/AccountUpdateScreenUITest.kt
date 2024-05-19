341b07978f1558f59919d1dd584cb411a9a93c09
Clément <clement.morena@epfl.ch>
2024-05-19 15:22:33 +0200

    more tests

--- app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/account/AccountUpdateScreenUITest.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/account/AccountUpdateScreenUITest.kt
@@ -3,14 +3,19 @@ package com.monkeyteam.chimpagne.newtests.ui.account
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.monkeyteam.chimpagne.model.database.Database
+import com.monkeyteam.chimpagne.newtests.SLEEP_AMOUNT_MILLIS
+import com.monkeyteam.chimpagne.newtests.TEST_ACCOUNTS
+import com.monkeyteam.chimpagne.newtests.initializeTestDatabase
 import com.monkeyteam.chimpagne.ui.account.AccountUpdateScreen
 import com.monkeyteam.chimpagne.viewmodels.AccountViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -22,21 +27,31 @@ class AccountUpdateScreenUITest {
 
   @get:Rule val composeTestRule = createComposeRule()
 
+  @Before
+  fun init() {
+    initializeTestDatabase()
+  }
+
   @Test
   fun testAccountUpdateScreen() {
 
     val accountViewModel = AccountViewModel(database = database)
 
+    accountViewModel.loginToChimpagneAccount(TEST_ACCOUNTS[0].firebaseAuthUID, {}, {})
+    while (accountViewModel.uiState.value.loading) {}
+
     var goBackPressed = false
     var accountModified = false
     composeTestRule.setContent {
-      AccountUpdateScreen(accountViewModel, { goBackPressed = true }, { accountModified = true })
+      AccountUpdateScreen(accountViewModel, { goBackPressed = true }, { accountModified = true }, editMode = true)
     }
 
     composeTestRule.onNodeWithTag("profile_icon").assertExists()
     composeTestRule.onNodeWithTag("first_name_label", useUnmergedTree = true).assertExists()
     composeTestRule.onNodeWithTag("last_name_label", useUnmergedTree = true).assertExists()
 
+    composeTestRule.onNodeWithTag("first_name_field").performTextClearance()
+    composeTestRule.onNodeWithTag("last_name_field").performTextClearance()
     composeTestRule.onNodeWithTag("submit_button").performClick()
     assertFalse(accountModified)
 
@@ -49,5 +64,9 @@ class AccountUpdateScreenUITest {
 
     composeTestRule.onNodeWithTag("go_back_button").performClick()
     assertTrue(goBackPressed)
+    composeTestRule.onNodeWithTag("submit_button").performClick()
+    while (accountViewModel.uiState.value.loading) {}
+    Thread.sleep(SLEEP_AMOUNT_MILLIS)
+    assertTrue(accountModified)
   }
 }
