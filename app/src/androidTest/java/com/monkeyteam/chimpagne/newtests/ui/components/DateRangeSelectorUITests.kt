cd865050ba79df79a0f69f660ad75aa303640fe2
Clément <clement.morena@epfl.ch>
2024-05-19 17:49:49 +0200

    ktfmtification

--- app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/components/DateRangeSelectorUITests.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/components/DateRangeSelectorUITests.kt
@@ -3,8 +3,6 @@ package com.monkeyteam.chimpagne.newtests.ui.components
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performScrollTo
-import androidx.compose.ui.test.performSemanticsAction
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.monkeyteam.chimpagne.model.utils.buildCalendar
 import com.monkeyteam.chimpagne.ui.components.DateRangeSelector
@@ -29,6 +27,8 @@ class DateRangeSelectorUITests {
     }
 
     composeTestRule.onNodeWithTag("date_range_button").performClick()
-    composeTestRule.onNodeWithTag("date_range_button").performClick() // performs the dismiss request
+    composeTestRule
+        .onNodeWithTag("date_range_button")
+        .performClick() // performs the dismiss request
   }
 }
--- app/src/main/java/com/monkeyteam/chimpagne/ui/components/DateRangeSelector.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/components/DateRangeSelector.kt
@@ -1,20 +1,11 @@
 package com.monkeyteam.chimpagne.ui.components
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.CalendarToday
-import androidx.compose.material3.Button
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDefaults
-import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DateRangePicker
-import androidx.compose.material3.DateRangePickerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SelectableDates
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,13 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.monkeyteam.chimpagne.R
 import com.monkeyteam.chimpagne.model.utils.setCalendarToMidnight
 import java.text.DateFormat
 import java.util.Calendar
@@ -45,37 +30,38 @@ fun DateRangeSelector(
   var showBottomSheet by remember { mutableStateOf(false) }
 
   val dateRangeState =
-    rememberDateRangePickerState(
-      initialSelectedStartDateMillis = startDate.timeInMillis,
-      initialSelectedEndDateMillis = endDate.timeInMillis,
-      selectableDates =
-      object : SelectableDates {
-        override fun isSelectableDate(utcTimeMillis: Long): Boolean {
-          val today = Calendar.getInstance()
-          setCalendarToMidnight(today)
+      rememberDateRangePickerState(
+          initialSelectedStartDateMillis = startDate.timeInMillis,
+          initialSelectedEndDateMillis = endDate.timeInMillis,
+          selectableDates =
+              object : SelectableDates {
+                override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                  val today = Calendar.getInstance()
+                  setCalendarToMidnight(today)
 
-          return utcTimeMillis >= today.timeInMillis
-        }
-      })
+                  return utcTimeMillis >= today.timeInMillis
+                }
+              })
 
   if (showBottomSheet) {
     ModalBottomSheet(
-      onDismissRequest = {
-        val newStartDate =
-          dateRangeState.selectedStartDateMillis?.let {
-            Calendar.getInstance().apply { timeInMillis = it }
-          } ?: Calendar.getInstance()
+        onDismissRequest = {
+          val newStartDate =
+              dateRangeState.selectedStartDateMillis?.let {
+                Calendar.getInstance().apply { timeInMillis = it }
+              } ?: Calendar.getInstance()
 
-        val newEndDate =
-          dateRangeState.selectedEndDateMillis?.let {
-            Calendar.getInstance().apply { timeInMillis = it }
-          } ?: newStartDate
+          val newEndDate =
+              dateRangeState.selectedEndDateMillis?.let {
+                Calendar.getInstance().apply { timeInMillis = it }
+              } ?: newStartDate
 
-        selectDateRange(newStartDate, newEndDate)
-        showBottomSheet = false
-      }, modifier = Modifier.testTag("date_range_submit")) {
-      DateRangePicker(state = dateRangeState)
-    }
+          selectDateRange(newStartDate, newEndDate)
+          showBottomSheet = false
+        },
+        modifier = Modifier.testTag("date_range_submit")) {
+          DateRangePicker(state = dateRangeState)
+        }
   }
 
   IconTextButton(
