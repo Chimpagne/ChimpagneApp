3ac226b2c68c9afc8c519099830eaedc45096a3c
Kuriv <gregory.richard@epfl.ch>
2024-05-20 09:10:10 +0200

    added missing imports

--- app/src/main/java/com/monkeyteam/chimpagne/ui/event/ChooseSocialsPanel.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/event/ChooseSocialsPanel.kt
@@ -1,8 +1,10 @@
 package com.monkeyteam.chimpagne.ui.event
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
