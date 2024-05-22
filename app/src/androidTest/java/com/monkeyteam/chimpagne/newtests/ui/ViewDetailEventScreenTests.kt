6d855cca33cc9c11ed0e4d485b67fcac04503d5a
Arnaud Levêque <arnaudlvq@gmail.com>
2024-05-22 14:17:23 +0200

    another one

--- app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/ViewDetailEventScreenTests.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/ViewDetailEventScreenTests.kt
@@ -1,7 +1,6 @@
 package com.monkeyteam.chimpagne.newtests.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -9,9 +8,6 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.monkeyteam.chimpagne.model.database.Database
@@ -19,9 +15,7 @@ import com.monkeyteam.chimpagne.newtests.TEST_ACCOUNTS
 import com.monkeyteam.chimpagne.newtests.TEST_EVENTS
 import com.monkeyteam.chimpagne.newtests.initializeTestDatabase
 import com.monkeyteam.chimpagne.ui.EventScreen
-import com.monkeyteam.chimpagne.ui.ManageStaffScreen
 import com.monkeyteam.chimpagne.ui.navigation.NavigationActions
-import com.monkeyteam.chimpagne.ui.navigation.Route
 import com.monkeyteam.chimpagne.viewmodels.AccountViewModel
 import com.monkeyteam.chimpagne.viewmodels.EventViewModel
 import org.junit.Before
@@ -166,34 +160,6 @@ class ViewDetailEventScreenTests {
 
   @OptIn(ExperimentalFoundationApi::class)
   @Test
-  fun testBedButton() {
-    // Logging in a user to an event he joined to display the buttons
-    val myAccount = TEST_ACCOUNTS[0]
-    accountManager.signInTo(myAccount)
-    val accountViewModel = AccountViewModel(database)
-    accountViewModel.loginToChimpagneAccount(myAccount.firebaseAuthUID, {}, {})
-    val event = TEST_EVENTS[4]
-    val eventVM = EventViewModel(event.id, database)
-    while (eventVM.uiState.value.loading) {}
-
-    composeTestRule.setContent {
-      val navController = rememberNavController()
-      val navActions = NavigationActions(navController)
-      EventScreen(navActions, eventVM, accountViewModel)
-    }
-
-    while (accountViewModel.uiState.value.loading) {}
-    while (eventVM.uiState.value.id.isEmpty()) {}
-
-    composeTestRule
-        .onNodeWithTag("bed_reservation")
-        .performScrollTo()
-        .assertHasClickAction()
-        .performClick()
-  }
-
-  @OptIn(ExperimentalFoundationApi::class)
-  @Test
   fun testParkingButton() {
     // Logging in a user to an event he joined to display the buttons
     val myAccount = TEST_ACCOUNTS[1]
@@ -214,55 +180,6 @@ class ViewDetailEventScreenTests {
     while (eventVM.uiState.value.id.isEmpty()) {}
 
     composeTestRule.onNodeWithTag("parking").performScrollTo().assertHasClickAction().performClick()
-  }
-
-  @OptIn(ExperimentalFoundationApi::class)
-  @Test
-  fun testSuppliesButton() {
-    // Logging in a user to an event he joined to display the buttons
-    val myAccount = TEST_ACCOUNTS[1]
-    accountManager.signInTo(myAccount)
-    val accountViewModel = AccountViewModel(database)
-    accountViewModel.loginToChimpagneAccount(myAccount.firebaseAuthUID, {}, {})
-    val event = TEST_EVENTS[1]
-    val eventVM = EventViewModel(event.id, database)
-    while (eventVM.uiState.value.loading) {}
-
-    composeTestRule.setContent {
-      val navController = rememberNavController()
-      val navActions = NavigationActions(navController)
-      EventScreen(navActions, eventVM, accountViewModel)
-    }
-
-    while (accountViewModel.uiState.value.loading) {}
-    while (eventVM.uiState.value.id.isEmpty()) {}
-
-    composeTestRule.onNodeWithTag("supplies").performScrollTo().assertHasClickAction()
-  }
-
-  @OptIn(ExperimentalFoundationApi::class)
-  @Test
-  fun testPollsButton() {
-
-    // Logging in a user to an event he joined to display the buttons
-    val myAccount = TEST_ACCOUNTS[0]
-    accountManager.signInTo(myAccount)
-    val accountViewModel = AccountViewModel(database)
-    accountViewModel.loginToChimpagneAccount(myAccount.firebaseAuthUID, {}, {})
-    val event = TEST_EVENTS[4]
-    val eventVM = EventViewModel(event.id, database)
-    while (eventVM.uiState.value.loading) {}
-
-    composeTestRule.setContent {
-      val navController = rememberNavController()
-      val navActions = NavigationActions(navController)
-      EventScreen(navActions, eventVM, accountViewModel)
-    }
-
-    while (accountViewModel.uiState.value.loading) {}
-    while (eventVM.uiState.value.id.isEmpty()) {}
-
-    composeTestRule.onNodeWithTag("polls").performScrollTo().assertHasClickAction().performClick()
   }
 
   @OptIn(ExperimentalFoundationApi::class)
@@ -291,30 +208,5 @@ class ViewDetailEventScreenTests {
         .performScrollTo()
         .assertHasClickAction()
         .performClick()
-  }
-
-  @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
-  @Test
-  fun testManageStaffButton() {
-    database.accountManager.signInTo(TEST_ACCOUNTS[1])
-    val event = TEST_EVENTS[0]
-    val eventVM = EventViewModel(event.id, database)
-    var navController: NavHostController? = null
-    while (eventVM.uiState.value.loading) {}
-
-    composeTestRule.setContent {
-      navController = rememberNavController()
-      val navActions = NavigationActions(navController!!)
-
-      NavHost(navController = navController!!, startDestination = Route.EVENT_SCREEN) {
-        composable(Route.EVENT_SCREEN) { EventScreen(navActions, eventVM, accountViewModel) }
-        composable(Route.MANAGE_STAFF_SCREEN + "/${eventVM.uiState.value.id}") {
-          ManageStaffScreen(
-              navObject = navActions, eventViewModel = eventVM, accountViewModel = accountViewModel)
-        }
-      }
-    }
-
-    composeTestRule.onNodeWithTag("manage staff").assertHasClickAction().performClick()
   }
 }
