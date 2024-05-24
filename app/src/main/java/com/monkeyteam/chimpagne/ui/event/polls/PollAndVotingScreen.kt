5bab18e517db7e229d7e5b65afdaf726371e3aed
juaniac <juan.iaconucci@epfl.ch>
2024-05-24 04:45:51 +0200

    fixed UI bug

--- app/src/main/java/com/monkeyteam/chimpagne/ui/event/polls/PollAndVotingScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/event/polls/PollAndVotingScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -101,7 +102,7 @@ fun PollsAndVotingScreen(eventViewModel: EventViewModel, onGoBack: () -> Unit) {
                 } else {
                   items(eventUIState.polls.values.toList()) { poll ->
                     ChimpagneButton(
-                        modifier = Modifier.testTag("a poll"),
+                        modifier = Modifier.fillMaxWidth().padding(5.dp).testTag("a poll"),
                         text = poll.title,
                         onClick = {
                           // TODO To be implemented in another PR//
