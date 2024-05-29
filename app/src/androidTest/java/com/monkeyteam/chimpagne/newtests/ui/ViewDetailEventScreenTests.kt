456dac2f779d019709d46d95a8211cf059e6ab00
Arnaud Levêque <arnaudlvq@gmail.com>
2024-05-29 19:07:36 +0200

    new tests

--- app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/ViewDetailEventScreenTests.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/newtests/ui/ViewDetailEventScreenTests.kt
@@ -269,7 +269,7 @@ class ViewDetailEventScreenTests {
     Thread.sleep(2000)
 
     composeTestRule
-        .onNodeWithTag("organiser", useUnmergedTree = true)
+        .onNodeWithTag("share", useUnmergedTree = true)
         .performScrollTo()
         .assertIsDisplayed()
   }
