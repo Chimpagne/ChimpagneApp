ed7d225053df0d8831727e7dce1180f7fda99bc9
Clément <clement.morena@epfl.ch>
2024-05-30 00:45:59 +0200

    update tests

--- app/src/androidTest/java/com/monkeyteam/chimpagne/end2end/EventCreation.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/end2end/EventCreation.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -92,6 +93,8 @@ class EventCreation {
     composeTestRule.waitUntil(timeout) {
       navController.currentDestination?.route == Route.HOME_SCREEN
     }
+    composeTestRule.onNodeWithTag("home_button").isDisplayed()
+    composeTestRule.onNodeWithTag("discover_events_button").isDisplayed()
     composeTestRule.onNodeWithTag("open_events_button").performClick()
 
     composeTestRule.waitUntil(timeout) {
