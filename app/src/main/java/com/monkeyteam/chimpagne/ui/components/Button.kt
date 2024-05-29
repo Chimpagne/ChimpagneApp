8efdbd215cf5a099d001247d1d616180b9a4992c
Clément <clement.morena@epfl.ch>
2024-05-29 09:18:41 +0200

    top bar and go back button

--- app/src/main/java/com/monkeyteam/chimpagne/ui/components/Button.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/components/Button.kt
@@ -53,7 +53,6 @@ import androidx.core.content.ContextCompat.startActivity
 import com.monkeyteam.chimpagne.R
 import com.monkeyteam.chimpagne.model.database.ChimpagneEvent
 import com.monkeyteam.chimpagne.model.utils.createCalendarIntent
-import com.monkeyteam.chimpagne.ui.navigation.NavigationActions
 import com.monkeyteam.chimpagne.ui.theme.ChimpagneFontFamily
 import com.monkeyteam.chimpagne.ui.theme.ChimpagneTypography
 import java.util.Locale
@@ -115,19 +114,19 @@ fun IconTextButton(
         Text(text.uppercase(Locale.ROOT), style = textStyle)
       }
 }
-
-@Deprecated(
-    "Use GoBackButton(onClick: () -> Unit) instead",
-    replaceWith = ReplaceWith("GoBackButton(onClick = onGoBack)"))
-@Composable
-fun GoBackButton(navigationActions: NavigationActions) {
-  IconButton(onClick = { navigationActions.goBack() }) {
-    Icon(
-        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-        contentDescription = "Go Back",
-        tint = MaterialTheme.colorScheme.onSurface)
-  }
-}
+//
+// @Deprecated(
+//    "Use GoBackButton(onClick: () -> Unit) instead",
+//    replaceWith = ReplaceWith("GoBackButton(onClick = onGoBack)"))
+// @Composable
+// fun GoBackButton(navigationActions: NavigationActions) {
+//  IconButton(onClick = { navigationActions.goBack() }) {
+//    Icon(
+//        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+//        contentDescription = "Go Back",
+//        tint = MaterialTheme.colorScheme.onSurface)
+//  }
+// }
 
 @Composable
 fun GoBackButton(onClick: () -> Unit) {
--- app/src/main/java/com/monkeyteam/chimpagne/ui/event/polls/PollAndVotingScreen.kt
+++ app/src/main/java/com/monkeyteam/chimpagne/ui/event/polls/PollAndVotingScreen.kt
@@ -11,17 +11,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Poll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -30,14 +27,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.monkeyteam.chimpagne.R
 import com.monkeyteam.chimpagne.model.database.ChimpagneRole
 import com.monkeyteam.chimpagne.ui.components.ChimpagneButton
+import com.monkeyteam.chimpagne.ui.components.GoBackButton
 import com.monkeyteam.chimpagne.ui.components.Legend
+import com.monkeyteam.chimpagne.ui.components.TopBar
 import com.monkeyteam.chimpagne.viewmodels.EventViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,18 +55,9 @@ fun PollsAndVotingScreen(eventViewModel: EventViewModel, onGoBack: () -> Unit) {
   }
   Scaffold(
       topBar = {
-        TopAppBar(
-            title = {
-              Text(
-                  stringResource(id = R.string.polls_topbar_title),
-                  Modifier.testTag("screen title"))
-            },
-            modifier = Modifier.shadow(4.dp),
-            navigationIcon = {
-              IconButton(onClick = { onGoBack() }) {
-                Icon(Icons.AutoMirrored.Filled.ArrowBack, "back")
-              }
-            })
+        TopBar(
+            text = stringResource(id = R.string.polls_topbar_title),
+            navigationIcon = { GoBackButton(onGoBack) })
       },
       floatingActionButton = {
         if (listOf(ChimpagneRole.OWNER, ChimpagneRole.STAFF)
