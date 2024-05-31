8b056cd5c208fac30a2f7528042bf151a3929a3d
Athanaze <sacha.liechti@arsent.ch>
2024-05-31 16:57:56 +0200

    Maybe a bigger timeout will make it work

--- app/src/androidTest/java/com/monkeyteam/chimpagne/end2end/f2.kt
+++ app/src/androidTest/java/com/monkeyteam/chimpagne/end2end/f2.kt
@@ -33,7 +33,7 @@ import org.junit.runner.RunWith
 /** End to end test for adding a supply to an event as well as creating a poll and voting on it. */
 @RunWith(AndroidJUnit4::class)
 class PollEnd2EndTest {
-  private val timeout: Long = 5000 // in milliseconds
+  private val timeout: Long = 50000 // in milliseconds
 
   val database = Database()
   val account =
